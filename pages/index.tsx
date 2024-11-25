fbf509250b19b81c714264747be5c3643ced9130
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-25 16:40:02 -0300

    Criação dos elementos, do visual e também da sua responsividade - Seção de apresentação, homeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/HomeNoAuth.module.scss";
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
+import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
 
 const HomeNotAuth = () => {
   return (
@@ -12,7 +13,10 @@ const HomeNotAuth = () => {
 			  <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
 			</Head>
 			<main>
-			<HeaderNoAuth/>
+				<div className={styles.sectionBackground}>
+					<HeaderNoAuth/>
+					<PresentationSection/>
+				</div>
 			</main>
 		</>
 )}
