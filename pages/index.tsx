8f668e64b834e8af5af88a50b102fd8e3d332d54
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-25 10:12:04 -0300

    Configuração do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import styles from "../styles/HomeNotAuth.module.scss";
+
+const HomeNotAuth = () => {
+  return (
+		<>
+			<Head><title>Home</title></Head>
+			<main></main>
+		</>
+)}
+
+export default HomeNotAuth;
