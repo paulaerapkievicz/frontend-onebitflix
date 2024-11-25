ac7b80f09db0b41b4b66183a8f0a7dfcba3acd89
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-25 15:22:05 -0300

    Criação do header da página homeNoAuth - Header Não autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from "next/head";
-import styles from "../styles/HomeNotAuth.module.scss";
+import styles from "../styles/HomeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNotAuth = () => {
   return (
 		<>
-			<Head><title>Home</title></Head>
-			<main></main>
+			<Head>
+				<title>Onebitflix</title>
+				<link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+				<meta property="og:title" content="Onebitflix" key="title" />
+			  <meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."></meta>
+			</Head>
+			<main>
+			<HeaderNoAuth/>
+			</main>
 		</>
 )}
 
