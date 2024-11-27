b35a9b00ef0138583938004cc1d8cd6a3d53fda0
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-11-27 18:10:18 -0300

    Criando a conexão do backend para login

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,8 +3,51 @@ import HeaderGeneric from "../src/components/common/headerGeneric";
 import styles from "../styles/registerLogin.module.scss";
 import { Container, Form, FormGroup, Label, Input, Button } from 'reactstrap'
 import Footer from "@/src/components/common/footer";
+import { useRouter } from "next/router";
+import { FormEvent, useEffect, useState } from "react";
+import ToastComponent from "@/src/components/toast";
+import authService from "@/src/services/authService";
 
 const Login = function () {
+  const router = useRouter();
+  const [toastColor, setToastColor] = useState("");
+  const [toastIsOpen, setToastIsOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+
+    useEffect(() => {
+      const registerSucess = router.query.registred;
+      if (registerSucess === "true") {
+        setToastColor("bg-success");
+        setToastIsOpen(true);
+        setTimeout(() => {
+          setToastIsOpen(false);
+        }, 1000 * 3);
+        setToastMessage("Cadatro feito com sucesso!");
+      }
+    }, [router.query]);
+
+    const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+    
+      const formData = new FormData(event.currentTarget);
+      const email = formData.get("email")!.toString();
+      const password = formData.get("password")!.toString();
+      const params = { email, password };
+
+      const { status } = await authService.login(params);
+
+      if (status === 200) {
+        router.push("/home");
+      } else {
+        setToastColor("bg-danger");
+        setToastIsOpen(true);
+        setTimeout(() => {
+          setToastIsOpen(false);
+        }, 1000 * 3);
+        setToastMessage("Email ou senha incorretos!");
+      }
+    }
+
   return (
     <>
       <Head>
@@ -15,7 +58,7 @@ const Login = function () {
         <HeaderGeneric logoUrl="/" btnUrl="/register" btnContent="Quero fazer parte" />
           <Container className="py-5">
             <p className={styles.formTitle}>Bem-vindo(a) de volta!</p>
-            <Form className={styles.form}>
+            <Form className={styles.form} onSubmit={handleLogin}>
               <p className="text-center">
                 <strong>Bem-vindo(a) ao OneBitFlix!</strong>
               </p>
@@ -33,8 +76,8 @@ const Login = function () {
                 />
               </FormGroup>
               <FormGroup>
-                <Label for="password" className={styles.label}>
                   SENHA
+                <Label for="password" className={styles.label}>
                 </Label>
                 <Input
                   id="password"
@@ -45,10 +88,11 @@ const Login = function () {
                   className={styles.input}
                 />
               </FormGroup>
-              <Button outline className={styles.formBtn}>
+              <Button type="submit" outline className={styles.formBtn}>
                 ENTRAR
               </Button>
             </Form>
+            <ToastComponent color={toastColor} isOpen={toastIsOpen} message={toastMessage}/>
           </Container>
         <Footer/>
       </main>
