8e12d768e8b8e53a44fffecb7560ab18a77f70ac
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-12-02 19:48:22 -0300

    Criação da página de Search

---
+++ pages/search.tsx
@@ -0,0 +1,67 @@
+import styles from "../styles/search.module.scss";
+import Head from "next/head";
+import HeaderAuth from "../src/components/common/headerAuth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import courseService, { CourseType } from "@/src/services/courseService";
+import PageSpinner from "@/src/components/common/spinner";
+import SearchCard from "@/src/components/searchCard";
+import { Container } from 'reactstrap'
+import Footer from "@/src/components/common/footer";
+
+
+const Search = function () {
+  const router = useRouter();
+  const searchName = router.query.name;
+  const [searchResult, setSearchResult] = useState<CourseType[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const searchCourses = async () => {
+    if (typeof searchName === 'string' && searchName.trim() !== '') { 
+      setLoading(true);
+      const res = await courseService.getSearch(searchName);
+      setSearchResult(res.data.courses);
+      setLoading(false);
+      // if (res.data.courses.length === 0) {
+      //   setSearchRender(false);
+      // }
+    }
+  };
+  
+
+  useEffect(() => {
+    searchCourses();
+  }, [searchName]);
+
+  return (
+    <>
+      <Head>
+        <title>Onebitflix - {searchName}</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      </Head>
+      <main className={styles.main}>
+      <div className={styles.headerFooterBg}>
+        <HeaderAuth/>
+      </div>
+      <div className={styles.searchResult}>
+        {loading ? (
+          <PageSpinner/>
+        ) : searchResult.length >= 1 ? (
+          <Container className="d-flex flex-wrap justify-content-center gap-5 py-4">
+          {searchResult?.map((course) => (
+            <SearchCard key={course.id} course={course} />
+          ))}
+        </Container>
+        ) : (
+          <p className={styles.noSearchResult}>Nenhum resultado encontrado!</p>
+        )}
+      </div>
+      <div className={styles.headerFooterBg}>
+        <Footer/>
+      </div>
+      </main>
+    </>
+  );
+};
+
+export default Search;
--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import styles from "./styles.module.scss";
 import { Container, Form, Input } from 'reactstrap'
 import Modal from "react-modal";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import profileService from "@/src/services/profileService";
 
@@ -12,6 +12,19 @@ const HeaderAuth = function () {
   const router = useRouter();
   const [modalOpen, setModalOpen] = useState(false);
   const [initials, setInitials] = useState("");
+  const [searchName, setSearchName] = useState("");
+
+  const handleSearch = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+      
+    router.push(`search?name=${searchName}`);
+    setSearchName("");
+  };
+  
+  const handleSearchClick = () => {
+    router.push(`search?name=${searchName}`);
+    setSearchName("");
+  };
 
   useEffect(() => {
     profileService.fetchCurrent().then((user) => {
@@ -21,18 +34,18 @@ const HeaderAuth = function () {
     });
   }, []);
 
-const handleOpenModal = () => {
-	setModalOpen(true);
-};
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
 
-const handleCloseModal = () => {
-	setModalOpen(false);
-};
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
-const handleLogout = () => {
-	sessionStorage.clear();
-  router.push("/");
-};
+  const handleLogout = () => {
+    sessionStorage.clear();
+    router.push("/");
+  };
 
   return (<>
     <Container className={styles.nav}>
@@ -40,15 +53,19 @@ const handleLogout = () => {
         <img src="/logoOnebitflix.svg" alt="logoOnebitflix" className={styles.imgLogoNav}/>
       </Link>
       <div className="d-flex align-items-center">
-      <Form>
+      <Form onSubmit={handleSearch}>
           <Input
             name="search"
             type="search"
             placeholder="Pesquisar"
             className={styles.input}
+            value={searchName}
+            onChange={(event) => {
+              setSearchName(event.currentTarget.value.toLowerCase());
+            }}
           />
         </Form>
-        <img src="/homeAuth/iconSearch.svg" alt="lupaHeader" className={styles.searchImg}/>
+        <img src="/homeAuth/iconSearch.svg" alt="lupaHeader" className={styles.searchImg} onClick={handleSearchClick}/>
         <p className={styles.userProfile} onClick={handleOpenModal}>{initials}</p>
       </div>
       <Modal
