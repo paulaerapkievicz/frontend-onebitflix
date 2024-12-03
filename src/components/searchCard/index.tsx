30f2523c1c79aa5d2def612639326c8d4849d520
Paula Rapkievicz <paularapkievicz@gmail.com>
2024-12-03 19:32:40 -0300

    Criação do visual da página de cursos e episódios

--- src/components/searchCard/index.tsx
+++ src/components/searchCard/index.tsx
@@ -8,7 +8,7 @@ interface props {
 
 const SearchCard = function ({ course }: props) {
   return (<>
-    <Link href={`/courses/${course.id}`}>
+    <Link href={`/course/${course.id}`}>
       <div className={styles.searchCard}>
         <img src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`} alt={course.name} className={styles.searchCardImg}/>
         <p className={styles.searchCardTitle}>{course.name}</p>
